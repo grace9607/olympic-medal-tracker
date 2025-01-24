7eecba0e42ee8371c942b4a9ff2e48eb7a331244
방은혜 <grace01099139607@gmail.com>
2025-01-24 10:00:47 +0900

    MedalList 컴포넌트에 주석 추가 및 코드 가독성 향상

--- src/MedalList.jsx
+++ src/MedalList.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import MedalItem from "./MedalItem";
+import MedalItem from "./MedalItem"; // 각 국가 데이터를 나타내는 컴포넌트
 import "./css/MedalList.css";
 
+// 국가 목록을 표시하는 컴포넌트 : 국가별 메달 데이터를 테이블 형식으로 출력, 삭제 기능을 제공
 const MedalList = ({ countries, deleteCountry }) => {
   return (
     <table className="table">
+      {/* 국가 목록 테이블 헤더 : 국가명, 금메달, 은메달, 동메달, 액션 */}
       <thead>
         <tr>
           <th>국가명</th>
@@ -14,13 +16,14 @@ const MedalList = ({ countries, deleteCountry }) => {
           <th>액션</th>
         </tr>
       </thead>
+      {/* 국가 목록 테이블 본문 : 국가별 메달 데이터 출력 */}
       <tbody>
         {countries.map((country, index) => (
           <MedalItem
-            key={index}
-            country={country}
-            index={index}
-            deleteCountry={deleteCountry}
+            key={index} // 고유 키로 인덱스 사용
+            country={country} // 국가 데이터 전달
+            index={index} // 현재 국가의 인덱스 전달
+            deleteCountry={deleteCountry} // 삭제 함수 전달
           />
         ))}
       </tbody>
